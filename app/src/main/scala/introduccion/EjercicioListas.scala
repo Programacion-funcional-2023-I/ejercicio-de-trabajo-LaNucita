d071bb9f8af3b827fd1bd232626468585342c66c
LaNucita <manuelsj2003@gmail.com>
2023-09-03 13:03:54 -0500

    Diagnostico PFC ejer 1,2,3

--- app/src/main/scala/introduccion/EjercicioListas.scala
+++ app/src/main/scala/introduccion/EjercicioListas.scala
@@ -12,10 +12,26 @@ class EjercicioListas {
   * @throws IllegalArgumentException si n es negativo
   */
   def repetirListas(lista: List[Int], n: Int): List[List[Int]] = {
-    var listaRepetida : List[List[Int]] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+    var listaRepetida: List[List[Int]] = List()
+    var numero_elementos: Int = lista.size
+    for (i <- 1 to numero_elementos) {
+      var listaInterna: List[Int] = List()
+      if (n == 0) {
+        listaRepetida = List() +: listaRepetida
+      }
+      else if (n > 0) {
+        for (j <- 1 to n) {
+          listaInterna = lista(i - 1) +: listaInterna
+        }
+        listaRepetida = listaInterna +: listaRepetida
+      }
+      else if (n < 0) {
+        throw new java.lang.IllegalArgumentException("No valores menores de 0")
+      }
+    }
+    return listaRepetida.reverse
   }
+
   /*
   * Punto 3: Filtrar listas
   * @param criterioIn Criterio de filtrado que puede ser "mayor", "menor", "mayoroigual", "igual", "diferente" o "menoroigual"
@@ -24,11 +40,23 @@ class EjercicioListas {
   * @return Lista filtrada de acuerdo al criterio y n
   * @throws IllegalArgumentException si el criterio no es uno de los valores válidos
   */
-
-  def filtrarListas(criterioIn: String, n: Int, lista: List[Int]) : List[Int] = {
-    var criterio : String = criterioIn.toLowerCase()
-    var listaFiltrada : List[Int] = List()
-    //Complete el código
-    throw new UnsupportedOperationException("No implementado aún")
+  def filtrarListas(criterioIn: String, n: Int, lista: List[Int]): List[Int] = {
+    val criterio = criterioIn.toLowerCase()
+    criterio match {
+      case "mayor" =>
+        lista.filter(_ > n)
+      case "menor" =>
+        lista.filter(_ < n)
+      case "mayoroigual" =>
+        lista.filter(_ >= n)
+      case "igual" =>
+        lista.filter(_ == n)
+      case "diferente" =>
+        lista.filter(_ != n)
+      case "menoroigual" =>
+        lista.filter(_ <= n)
+      case _ =>
+        throw new IllegalArgumentException("Criterio no válido")
+    }
   }
 }
